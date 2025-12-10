eb3cf18880f227256b5de37a748fea213cb96d08
Nikolay Sheremet <sheremet.san@gmail.com>
2025-12-10 22:38:07 +0300

    Проходил обучение по модулю делаю второе задание

--- step1_create.py
+++ step1_create.py
@@ -1,17 +1,32 @@
 from openpyxl import Workbook
+import random
 
-# создаём новую книгу
+coutn = 0
 wb = Workbook()
+ws = wb.active
+ws.title = "Данные"
 
-# выбираем активный лист
-#ws = wb.active
+ws_a = []
 
-# записываем текст в ячейку A1
-#ws["A1"] = "Привет, Excel!"
-#wb.create_sheet(title='Книга 0', index=0)
-list_s = ['A','B','C']
-for i in list_s:
-    wb.create_sheet(i)
-# сохраняем файл
-wb.save("Проверка.xlsx")
-print(wb.sheetnames)
+for row in range(1,51):
+    ws[f"A{row}"] = random.randint(10,998)
+    ws_a.append(ws[f"A{row}"].value)
+    
+max_value = max(ws_a)
+min_value = min(ws_a)
+total = sum(ws_a)
+
+
+print(max_value)
+print(min_value)
+print(total)
+
+
+
+stat = wb.create_sheet("Статистика")
+    
+stat["A1"] = f"Максимум: {max_value}"
+stat["A2"] = f"Минимум: {min_value}"
+stat["A3"] = f"Сумма: {total}"
+    
+wb.save("домашка_1.xlsx")
